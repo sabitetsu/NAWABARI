a6f11ae5273aec412326c81877083d698d1955a1
KYO925 <73161681+KYO925@users.noreply.github.com>
2021-07-10 16:35:57 +0900

    Update test.py

--- test.py
+++ test.py
@@ -6,10 +6,32 @@ Config.set('graphics', 'height', '480')
 from kivy.app import App
 
 from kivy.uix.widget import Widget
-from kivy.properties import StringProperty
+from kivy.properties import StringProperty, ListProperty
 
 class MyWidget(Widget):
-    pass
+    text  = StringProperty()
+    color = ListProperty([1,1,1,1])
+    count = 0
+
+    def __init__(self, **kwargs):
+        super(MyWidget, self).__init__(**kwargs)
+        self.text = 'start'
+
+    def buttonClicked(self):
+        self.count += 1
+        if self.count == 1:
+            self.text = 'なんで押すんだよ'
+            self.color = [1, 0, 0, 1]
+        elif self.count == 2:
+            self.text = 'おい'
+        elif self.count == 3:
+            self.text = 'だから'
+        elif self.count == 4:
+            self.text = 'なんで'
+        elif self.count == 5:
+            self.text = '押すんだよ'
+        elif self.count > 5:
+            self.text = 'おい！！！！'
 
 class TestApp(App):
 
