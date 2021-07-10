929180811733bb6de8caee261e58b38e3e3fea77
KYO925 <73161681+KYO925@users.noreply.github.com>
2021-07-10 16:15:58 +0900

    Update test.py

--- test.py
+++ test.py
@@ -1,8 +1,25 @@
 #-*- coding: utf-8 -*-
+from kivy.config import Config
+Config.set('graphics', 'width', '640')
+Config.set('graphics', 'height', '480')
+
 from kivy.app import App
 
-class TestApp(App):
+from kivy.uix.widget import Widget
+from kivy.properties import StringProperty
+
+class MyWidget(Widget):
     pass
+
+class TestApp(App):
+
+    def __init__(self, **kwargs):
+        super(TestApp, self).__init__(**kwargs)
+        self.title = 'greeting'    # ウィンドウの名前を変更
+
+        def build(self):
+            return MyWidget()
+
 
 if __name__ == '__main__':
     TestApp().run()
