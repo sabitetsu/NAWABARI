f1f7a62916651ed2c1bd6af740a5c81985bebe4e
Watanabe <64056412+WATANA-be@users.noreply.github.com>
2021-07-11 10:34:47 +0900

    map_add

--- aaaa.py
+++ aaaa.py
@@ -3,7 +3,7 @@ from kivy.app import App
 
 class MapViewApp(App):
     def build(self):
-        mapview = MapView(xoom=17,lat=35.681382,lon=139.766084)#ここにつばきちの位置情報を入れたい
+        mapview = MapView(zoom=15, lat=35.681382, lon=139.766084)
         return mapview
 
-MapViewApp().run
+MapViewApp().run()
