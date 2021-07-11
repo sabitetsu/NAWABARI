42118225e203fc84b662099dbf3a37406e912067
Watanabe <64056412+WATANA-be@users.noreply.github.com>
2021-07-11 12:33:42 +0900

    kill_python

--- aaaa.py
+++ aaaa.py
@@ -1,9 +1,11 @@
-from kivy.garden.mapview import MapView
+from kivy.garden.mapview import MapView, MapMarkerPopup
 from kivy.app import App
-
+import locationtest
 class MapViewApp(App):
     def build(self):
-        mapview = MapView(zoom=15, lat=35.681382, lon=139.766084)
+        mapview = MapView(zoom=50, lat=35.681382, lon=139.766084)
+        marker1 = MapMarkerPopup(lat=35.681382, lon=139.766084) 
+        mapview.add_marker(marker1)
         return mapview
 
-MapViewApp().run()
+new_var = MapViewApp().run()
